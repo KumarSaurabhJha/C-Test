31718feec755036df08574da3f275aed8fd1e368
saurabhkumar <k.saurabh@vereinigte-hagel.de>
2022-03-20 21:19:43 +0100

    fetch data every 30 seconds

--- app/src/main/java/com/cariad/test/presentation/viewmodel/POIViewModel.kt
+++ app/src/main/java/com/cariad/test/presentation/viewmodel/POIViewModel.kt
@@ -1,10 +1,44 @@
 package com.cariad.test.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.cariad.test.domain.usecases.GetPOIDataUseCase
+import com.cariad.test.presentation.util.Event
+import com.kumar.test.data.model.POIData
+import com.kumar.test.data.model.POIRequestHeaders
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class POIViewModel(
     private val getPOIDataUseCase: GetPOIDataUseCase
 ) : ViewModel() {
+    private val _poiDisplayList = MutableLiveData<Event<POIData>>()
+    val poiDisplayList: LiveData<Event<POIData>> get() = _poiDisplayList
+
+
+    val fetchDataJob = fetchPOIListJob()
+
+    fun cancelDataFetch() {
+        fetchDataJob.cancel()
+    }
+
+    private fun fetchPOIListJob(): Job =
+        viewModelScope.launch(Dispatchers.IO) {
+            while (true) {
+                fetchPOIList()
+                delay(TimeUnit.SECONDS.toMillis(30))
+            }
+        }
+
+
+    private suspend fun fetchPOIList() {
+        val list = getPOIDataUseCase.execute(POIRequestHeaders())
+        _poiDisplayList.postValue(Event(list))
+    }
 
 }
